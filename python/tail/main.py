5915e085df87a234ad35800229bf8bea43f12cd6
Michael Tal <zarathir@proton.me>
2023-03-14 18:13:52 +0100

    extend list files, create tail program

---
+++ python/tail/main.py
@@ -0,0 +1,12 @@
+import os
+import subprocess
+
+
+if __name__ == "__main__":
+
+    tail = subprocess.Popen(["tail", "-f", "logdatei"], cwd=os.getcwd(), )
+
+    while not tail.stderr:
+        out = tail.stdout
+        if out is not None:
+            print(tail.stdout)
