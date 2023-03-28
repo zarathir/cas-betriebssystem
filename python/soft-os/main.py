6bdf04e32994c08fd3e48c4add1f48facdfb33ce
Michael Tal <zarathir@proton.me>
2023-03-28 20:09:28 +0200

    extend os

--- python/soft-os/main.py
+++ python/soft-os/main.py
@@ -1,13 +1,44 @@
+NOP = 0x00
+ADD = 0x01
+SUB = 0x02
+JMP = 0x03
+HLT = 0xFF
+
+PROGRAM = [
+    ADD, 0x00, 0x01,
+    JMP, 0x06,
+    
+    HLT,
+
+    ADD, 0x01, 0x01,
+    JMP, 0x00,
+    
+    HLT
+]
 
 if __name__ == "__main__":
-    RAM = ['NOP', 'NOP', 'BLABLA']
+    register = [0] * 8
 
     instruction_pointer = 0
 
     while True:
-        op = RAM[instruction_pointer]
+        operation = PROGRAM[instruction_pointer]
 
-        if op == 'NOP':
-            print("Doing nothing")
+        if operation == NOP:
+            instruction_pointer += 1
+            pass
+
+        elif operation == ADD:
+            register[PROGRAM[instruction_pointer + 1]] += register[PROGRAM[instruction_pointer + 2]]
+            instruction_pointer += 3
+                
+        elif operation == SUB:
+            register[PROGRAM[instruction_pointer + 1]] -= register[PROGRAM[instruction_pointer + 2]]
+            instruction_pointer += 3
         
-        instruction_pointer += 1
+        elif operation == JMP:
+            instruction_pointer = PROGRAM[instruction_pointer + 1]
+
+        elif operation == HLT:
+            instruction_pointer += 1
+            exit
