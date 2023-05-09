64a95c7c62781b76e473ef3854c5097c8f9fec46
Michael Tal <zarathir@proton.me>
2023-05-09 15:54:32 +0200

    handle interrupt and refactor to cpu class

--- python/soft-os/main.py
+++ python/soft-os/main.py
@@ -2,6 +2,7 @@ import threading
 import time
 
 INTERRUPT_INTERVAL = 5  # in Sekunden
+INTERRUPT_OFFSET = 10
 interrupt = False
 
 NOP = 0x00
@@ -10,7 +11,58 @@ SUB = 0x02
 JMP = 0x03
 HLT = 0xFF
 
-PROGRAM = [ADD, 0x00, 0x01, JMP, 0x06, HLT, ADD, 0x01, 0x01, JMP, 0x00, HLT]
+PROGRAM = [ADD, 0x00, 0x01, JMP, 0x06, HLT, ADD, 0x01, 0x01, JMP, 0x00, NOP, HLT]
+
+class Cpu(threading.Thread):
+
+    def __init__(self):
+        threading.Thread.__init__(self)
+        self.instruction_pointer = 0
+        self.register = [0] * 8
+        self.program = PROGRAM
+
+    def handle_interrupt(self):
+        global interrupt
+        
+        if interrupt is True:
+            instruction_pointer_old = self.instruction_pointer
+            self.instruction_pointer = INTERRUPT_OFFSET
+            self.handle_instruction()
+            self.instruction_pointer = instruction_pointer_old
+
+            interrupt = False
+
+    def handle_instruction(self):
+            operation = PROGRAM[self.instruction_pointer]
+
+            if operation == NOP:
+                print("Doing nothing")
+                self.instruction_pointer += 1
+                pass
+
+            elif operation == ADD:
+                self.register[PROGRAM[self.instruction_pointer + 1]] += self.register[
+                    PROGRAM[self.instruction_pointer + 2]
+                ]
+                self.instruction_pointer += 3
+
+            elif operation == SUB:
+                self.register[PROGRAM[self.instruction_pointer + 1]] -= self.register[
+                    PROGRAM[self.instruction_pointer + 2]
+                ]
+                self.instruction_pointer += 3
+
+            elif operation == JMP:
+                self.instruction_pointer = PROGRAM[self.instruction_pointer + 1]
+
+            elif operation == HLT:
+                exit(0)
+
+    def run(self):
+        while True:
+            self.handle_instruction()
+            self.handle_interrupt()
+
 
 def interrupt_controller_thread():
     global interrupt
@@ -21,53 +73,8 @@ def interrupt_controller_thread():
         interrupt = True
 
 
-def cpu_handle_interrupt():
-    global interrupt
-
-    if interrupt is True:
-        print("Woah I got an interrupt")
-        # now act on the interrupt...
-        interrupt = False
-
-
-def cpu_thread():
-    while True:
-        register = [0] * 8
-
-        instruction_pointer = 0
-
-        while True:
-            operation = PROGRAM[instruction_pointer]
-
-            if operation == NOP:
-                instruction_pointer += 1
-                pass
-
-            elif operation == ADD:
-                register[PROGRAM[instruction_pointer + 1]] += register[
-                    PROGRAM[instruction_pointer + 2]
-                ]
-                instruction_pointer += 3
-
-            elif operation == SUB:
-                register[PROGRAM[instruction_pointer + 1]] -= register[
-                    PROGRAM[instruction_pointer + 2]
-                ]
-                instruction_pointer += 3
-
-            elif operation == JMP:
-                instruction_pointer = PROGRAM[instruction_pointer + 1]
-
-            elif operation == HLT:
-                instruction_pointer += 1
-                exit(0)
-
-            time.sleep(1)
-            cpu_handle_interrupt()
-
-
 if __name__ == "__main__":
-    cpu = threading.Thread(target=cpu_thread)
+    cpu = Cpu()
     pic = threading.Thread(target=interrupt_controller_thread)
 
     cpu.start()
